825c45e8879ddf091fc2dc46a3c3527e89971b8a
Sasha <st087955@student.spbu.ru>
2026-03-15 00:17:09 +0300

    Создана страница с приветствием, добавлены основные стили
    Создан компонент greeting-page. Отображается приветствие и ссылка на
    менюшки. Применены стили. Добавлена анимация кнопки.
    В основные стили сохранены главные цвета. Настроены основные стили:
    шрифт, цвета текста и фона.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GreetingPage } from "./pages/greeting-page/greeting-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GreetingPage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/pages/greeting-page/greeting-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-greeting-page',
+  imports: [],
+  templateUrl: './greeting-page.html',
+  styleUrl: './greeting-page.scss',
+})
+export class GreetingPage {}
